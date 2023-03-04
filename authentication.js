c6199787bbde47ed1116699bf2b8a717e08eac0d
tinnamchoi <tinnam.choi@gmail.com>
2023-03-04 20:48:04 +1030

    removed unnecessary scopes

--- authentication.js
+++ authentication.js
@@ -1,6 +1,6 @@
 var client_id = 'f92fd28be38945ba94229da71cd26566';
 var redirect_uri = window.location.href;
-var scope = 'playlist-read-private user-top-read';
+var scope = 'user-top-read';
 var url = 'https://accounts.spotify.com/authorize';
 
 url += '?response_type=token';
