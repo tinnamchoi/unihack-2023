388fd3ef9fbfda9f4085d53dd56f002d5e1c3026
tinnamchoi <tinnam.choi@gmail.com>
2023-03-04 19:07:52 +1030

    extracting genres from top artists

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-var stateKey = 'spotify_auth_state';
-
-function generateRandomString(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-
 var client_id = 'f92fd28be38945ba94229da71cd26566';
 var redirect_uri = 'http://tinnamchoi.github.io/unihack-2023/callback';
 
-var state = generateRandomString(16);
-
-localStorage.setItem(stateKey, state);
-var scope = '';
+var scope = 'playlist-read-private user-top-read';
 
 var url = 'https://accounts.spotify.com/authorize';
 url += '?response_type=token';
 url += '&client_id=' + encodeURIComponent(client_id);
 url += '&scope=' + encodeURIComponent(scope);
 url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-url += '&state=' + encodeURIComponent(state);
--- callback.js
+++ callback.js
@@ -10,6 +10,27 @@ function httpGet(theUrl) {
   return xmlHttp.responseText;
 }
 
-var response = JSON.parse(httpGet("https://api.spotify.com/v1/me"));
+var response = JSON.parse(httpGet("https://api.spotify.com/v1/me/top/artists?limit=50"));
 
-document.getElementById("display_name").innerHTML = response.display_name;
+console.log(response);
+
+// create string array of const genres
+var genres = ["punk", "jazz", "classical", "alternative rock", "rock", "reggae", "ambient", "pop", "metal", "hip-hop", "edm", "blues", "country", "soul"];
+var count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// in each item, count each genre by checking if string containing one of the genres array element
+for (var i = 0; i < response.items.length; i++) {
+  for (var j = 0; j < response.items[i].genres.length; j++) {
+    for (var k = 0; k < genres.length; k++) {
+      if (response.items[i].genres[j].includes(genres[k])) {
+        count[k]++;
+        break;
+      }
+    }
+  }
+}
+
+// print out the count array
+for (var i = 0; i < count.length; i++) {
+  console.log(genres[i] + ": " + count[i]);
+}
