e57cae3a3e717ffa56a029c29b6f8dfa6610ff7d
tinnamchoi <tinnam.choi@gmail.com>
2023-03-04 14:30:33 +1030

    Spotify login redirect

---
+++ app.js
@@ -0,0 +1,26 @@
+var stateKey = 'spotify_auth_state';
+
+function generateRandomString(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var client_id = 'f92fd28be38945ba94229da71cd26566';
+var redirect_uri = 'http://localhost:8080/callback';
+
+var state = generateRandomString(16);
+
+localStorage.setItem(stateKey, state);
+var scope = 'user-read-private user-read-email';
+
+var url = 'https://accounts.spotify.com/authorize';
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(client_id);
+url += '&scope=' + encodeURIComponent(scope);
+url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+url += '&state=' + encodeURIComponent(state);
