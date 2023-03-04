03329a29ddebd947917337ddf7e1147724b6f447
tinnamchoi <tinnam.choi@gmail.com>
2023-03-04 16:39:27 +1030

    display display_name

--- callback.js
+++ callback.js
@@ -2,4 +2,14 @@ var hash = window.location.hash;
 
 var access_token = hash.split('&')[0].split('=')[1];
 
-console.log(access_token);
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.setRequestHeader("Authorization", "Bearer " + access_token);
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+
+var response = JSON.parse(httpGet("https://api.spotify.com/v1/me"));
+
+document.getElementById("display_name").innerHTML = response.display_name;
